33fdd4aefc34850f9e754aac84677f63b0066406
leidyFinaktiva <leidy.rueda@kinaktiva.com>
2023-07-25 15:26:23 -0500

    Se suben parámetros de configuración de producción.

--- AWSLambdaConsumoERP.Application/Resources/Constantes.cs
+++ AWSLambdaConsumoERP.Application/Resources/Constantes.cs
@@ -3,11 +3,11 @@
     public static class Constantes
     {
         public const string SIGNATURE = "HMAC-SHA256";
-        public const string CUSTOMER_KEY = "be5b034036a07ff3129b8843f50017cc5745049af8484430c793b1abc827eb75";
-        public const string SECRET = "960f8b86cfedbb1437cd23c2175299d8a5c8c3278a74a612dbb5456b7012b92c";
-        public const string ACCESS_TOKEN= "04f81fc685c69202d23715c23c1ffe62c161cd8778148bbd4e810fb4d5efcec2";
-        public const string TOKEN= "cf7e5aaa9da6c41d7869be1f60c2f7cd10db819ce83367d77dab40a674351c38";
-        public const string REALM = "7575517_SB1";
+        public const string CUSTOMER_KEY = "3542ab8cb783fed0627182fd53fc42881780e08c6ea5561e402e0338944fd44a";
+        public const string SECRET = "85fb9b2f659d338a43ae27b696c8ed82877bc779e6ed4ca3c8a14735f8eb8592";
+        public const string ACCESS_TOKEN= "273b22304b15ed2ae0a0a522bd56fc75b1949407cc35ef903909eaa74825daca";
+        public const string TOKEN= "01a7b5fe27c0aa319f1a7ea25d2b33884a93b42102e232eb53fdf63a8ae9d307";
+        public const string REALM = "7575517";
         public const string VERSION = "1.0";
     }
 }
